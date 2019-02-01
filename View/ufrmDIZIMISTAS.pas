eea0a9f600aae0a26a09102c0c1232a2c2020790
Daniel Ramos <daniel.henrique@tron.com.br>
2019-02-01 13:47:46 -0200

    01/02/2019

--- View/ufrmDIZIMISTAS.pas
+++ View/ufrmDIZIMISTAS.pas
@@ -6,7 +6,7 @@ uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, udmDIZIMAN, Data.DB, Vcl.Buttons,
   Vcl.ExtCtrls, System.Actions, Vcl.ActnList, System.ImageList,
-  Vcl.ImgList, Vcl.Grids, Vcl.DBGrids;
+  Vcl.ImgList, Vcl.Grids, Vcl.DBGrids, Vcl.ComCtrls;
 
 type
   TfrmDIZIMISTAS = class(TForm)
@@ -16,6 +16,26 @@ type
     dstabela: TDataSource;
     Panel1: TPanel;
     SpeedButton1: TSpeedButton;
+    PageControl1: TPageControl;
+    pcCADASTRO: TTabSheet;
+    pcPESQUISAR: TTabSheet;
+    SpeedButton2: TSpeedButton;
+    SpeedButton3: TSpeedButton;
+    SpeedButton4: TSpeedButton;
+    SpeedButton5: TSpeedButton;
+    SpeedButton6: TSpeedButton;
+    SpeedButton7: TSpeedButton;
+    SpeedButton8: TSpeedButton;
+    acINSERIR: TAction;
+    acEDITAR: TAction;
+    acFECHAR: TAction;
+    acCANCELAR: TAction;
+    acEXCLUIR: TAction;
+    acPESQUISAR: TAction;
+    acIMPRIMIR: TAction;
+    acSALVAR: TAction;
+    SpeedButton9: TSpeedButton;
+    SpeedButton10: TSpeedButton;
   private
     { Private declarations }
   public
