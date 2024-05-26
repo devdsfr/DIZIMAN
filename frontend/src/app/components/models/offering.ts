a2e12d93624011e312989e2b9aefc39c8f77164a
Daniel Ramos <44585550+devdsfr@users.noreply.github.com>
2024-05-25 22:07:03 -0300

    front end

--- frontend/src/app/components/models/offering.ts
+++
@@ -1,7 +0,0 @@
-export class Offering {
-  id?: number;
-  value?: number;
-  churchProject?: string;
-  offeringDate?: Date;
-  memberId?: number; // Assuming the offering links to a member by ID
-}
